b88e29c8726801c3c38d4b53a6bf713d12fb9d3c
Drikus-Bothma <231155@virtualwindow.co.za>
2023-05-28 21:11:41 +0200

    update

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ formInfo = () => {
     if(exampleCheck1.checked){
         alert("Welcome " + nameSurname + "! Thank you for your interest in " + subject + "! We will contact you via the email provided: " + email + ", or alternatively on the phone number: " + pNumber + ".")
     } else {
-        alert("Well, well, well... why don't you want to sign up for our newsletter... you don't seem to be that interested Name & Surname!  If you want to sign up, you better CHECK that newsletter box!")
+        alert("Well, well, well... why don't you want to sign up for our newsletter... you don't seem to be that interested " + nameSurname + "! If you want to sign up, you better CHECK that newsletter box!")
     }
 
     document.getElementById("signUpForm").reset()
